3f71d6628e242f57e785d0c2a7344cfc2b271b21
Ahmed Maghrabi <ahmedhisham12351@gmail.com>
2022-11-12 22:26:11 +0300

    failing push

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://localhost:8080/api/v0',
 };
+console.log('hello world inside here')
 
 /*
  * For easier debugging in development mode, you can import the following file
